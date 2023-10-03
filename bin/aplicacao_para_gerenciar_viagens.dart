900eaf06c7877bb83ba31df96955f75f8164b701
Alan Kardec <alankardec05@gmail.com>
2023-10-03 18:03:47 -0300

    Criando enum e metodos

--- bin/aplicacao_para_gerenciar_viagens.dart
+++ bin/aplicacao_para_gerenciar_viagens.dart
@@ -1,4 +1,46 @@
+void main() {
+  escolherMeioTransporte(Transporte.skate);
 
-void main(){
+  Set<String> registrosVisitados = <String>{};
 
+  registrosVisitados = registrarDestinos("Rio de Janeiro",registrosVisitados);
+  registrosVisitados = registrarDestinos("Belem",registrosVisitados);
+  registrosVisitados = registrarDestinos("Manaus",registrosVisitados);
+  registrosVisitados = registrarDestinos("Curitiba",registrosVisitados);
+  registrosVisitados = registrarDestinos("Rio de Janeiro",registrosVisitados);
+
+  print(registrosVisitados);
+
+}
+
+Set<String> registrarDestinos(String destino, Set<String> banco){
+  banco.add(destino);
+  return banco;
+}
+
+void escolherMeioTransporte (Transporte locomocao){
+  switch (locomocao) {
+    case Transporte.carro:
+      print("Vou de CARRO para aventura!");
+      break;
+    case Transporte.bike:
+      print("Vou de BIKE para aventura!");
+      break;
+    case Transporte.aviao:
+      print("Vou de AVIAO para aventura!");
+      break;
+    default:
+      print("Estou indo para aventura, isso que importa!");
+  }
+
+}
+
+enum Transporte{
+  carro,
+  bike,
+  andando,
+  skate,
+  aviao,
+  patins,
+  trem,
 }
