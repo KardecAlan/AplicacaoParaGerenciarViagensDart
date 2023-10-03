c67bec1316fc78a90908f63c7f36d83390b764fb
Alan Kardec <alankardec05@gmail.com>
2023-10-03 17:04:55 -0300

    Criando repositorio

---
+++ test/aplicacao_para_gerenciar_viagens_test.dart
@@ -0,0 +1,8 @@
+import 'package:aplicacao_para_gerenciar_viagens/aplicacao_para_gerenciar_viagens.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
